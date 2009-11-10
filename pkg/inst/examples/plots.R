f1a82ab6557c2c11a8daa5752f61a35f3689ab5d
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-10 14:52:13 +0000

    Simple example that shows how to create plot files from workers.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inst/examples/plots.R
@@ -0,0 +1,23 @@
+library(doMPI)
+
+cl <- startMPIcluster(count=2, verbose=TRUE)
+registerDoMPI(cl)
+
+trials <- 10
+n <- nrow(iris)
+leaveout <- 2
+chunkSize <- ceiling(trials / getDoParWorkers())
+mpiopts <- list(chunkSize=chunkSize)
+
+trash <- function(...) NULL
+foreach(i=icount(trials), .combine=trash, .multicombine=TRUE,
+        .packages='randomForest', .options.mpi=mpiopts) %dopar% {
+  d <- iris[sample(n, n - leaveout),]
+  rf <- randomForest(Species~., data=d, proximity=TRUE)
+  png(filename=sprintf('MDSplot_%d.png', i))
+  MDSplot(rf, d$Species)
+  dev.off()
+  NULL
+}
+
+closeCluster(cl)
