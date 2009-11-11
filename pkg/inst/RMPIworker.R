2932fd7a31995bc18006abb9c23747aff64fae69
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-11 01:59:22 +0000

    Added header material.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/RMPIworker.R
+++ pkg/inst/RMPIworker.R
@@ -1,3 +1,21 @@
+#
+# Copyright (c) 2009, Stephen B. Weston
+#
+# This is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+# USA
+
 local({
   # set default option values
   workdir <- Sys.getenv('TMPDIR', '/tmp')
@@ -65,6 +83,7 @@ local({
     cat('Error computing outfile\n', file=stderr())
   }
 
+  # redirect stdout, stderr, warnings, etc, to outfile
   doMPI:::sinkWorkerOutput(outfile)
 
   procname <- mpi.get.processor.name()
@@ -72,7 +91,8 @@ local({
 
   if (verbose) {
     cat("Starting MPI worker\n")
-    cat(sprintf("Worker processor name: %s; nodename: %s\n", procname, nodename))
+    cat(sprintf("Worker processor name: %s; nodename: %s\n",
+                procname, nodename))
   }
 
   # get the nodename of all the workers
