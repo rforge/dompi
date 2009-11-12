9041e4e00dad0e69cd20df5d349a37d291b3340e
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-12 21:02:15 +0000

    Added a benchmarks directory with the previous bootstrap example as the first benchmark.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inst/benchmarks/bootstrapMPI.R
@@ -0,0 +1,51 @@
+library(doMPI)
+
+usenws <- as.logical(Sys.getenv('USENWS', 'FALSE'))
+count <- as.integer(Sys.getenv('COUNT', '2'))
+verbose <- as.logical(Sys.getenv('VERBOSE', 'FALSE'))
+im <- as.logical(Sys.getenv('INCLUDEMASTER', 'TRUE'))
+
+cl <- if (usenws) {
+  startNWScluster(count=count, verbose=verbose, includemaster=im)
+} else {
+  startMPIcluster(count=count, verbose=verbose, includemaster=im)
+}
+registerDoMPI(cl)
+
+x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+trials <- 10000
+
+chunking <- as.logical(Sys.getenv('CHUNKING', 'TRUE'))
+chunkSize <- if (chunking) ceiling(trials / getDoParWorkers()) else 1
+mpiopts <- list(chunkSize=chunkSize)
+
+ptime <- system.time({
+  r <- foreach(icount(trials), .combine=cbind, .options.mpi=mpiopts) %dopar% {
+    ind <- sample(100, 100, replace=TRUE)
+    result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+    coefficients(result1)
+  }
+})[3]
+
+cat(sprintf('Parallel time using doMPI on %d workers: %f\n',
+            getDoParWorkers(), ptime))
+
+closeCluster(cl)
+if (!usenws) {
+  mpi.finalize()
+}
+
+sequential <- as.logical(Sys.getenv('SEQUENTIAL', 'FALSE'))
+if (sequential) {
+  stime <- system.time({
+    r <- foreach(icount(trials), .combine=cbind) %do% {
+      ind <- sample(100, 100, replace=TRUE)
+      result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+      coefficients(result1)
+    }
+  })[3]
+
+  cat(sprintf('Sequential time: %f\n', stime))
+  cat(sprintf('Speed up for %d workers: %f\n',
+              getDoParWorkers(), round(stime / ptime, digits=2)))
+}
