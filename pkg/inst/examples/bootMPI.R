3b3e4081e55738366bdb942f50813e3868501494
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-12 20:47:38 +0000

    Converted bootMPI.R from a benchmark to an example.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/bootMPI.R
+++ pkg/inst/examples/bootMPI.R
@@ -1,48 +1,27 @@
 library(doMPI)
 
-usenws <- as.logical(Sys.getenv('USENWS', 'FALSE'))
-verbose <- as.logical(Sys.getenv('VERBOSE', 'FALSE'))
-im <- as.logical(Sys.getenv('INCLUDEMASTER', 'TRUE'))
-
-cl <- if (usenws) {
-  startNWScluster(workerCount=2, verbose=verbose, includemaster=im)
-} else {
-  startMPIcluster(count=2, verbose=verbose, includemaster=im)
-}
+# create and register a doMPI cluster
+cl <- startMPIcluster(2)
 registerDoMPI(cl)
 
+# initialize variables
 x <- iris[which(iris[,5] != "setosa"), c(1,5)]
 trials <- 10000
 
-chunking <- as.logical(Sys.getenv('CHUNKING', 'TRUE'))
-chunkSize <- if (chunking) ceiling(trials / getDoParWorkers()) else 1
+# define chunkSize so that each cluster worker gets a single "task chunk"
+chunkSize <- ceiling(trials / getDoParWorkers())
 mpiopts <- list(chunkSize=chunkSize)
 
-ptime <- system.time({
-  r <- foreach(icount(trials), .combine=cbind, .options.mpi=mpiopts) %dopar% {
-    ind <- sample(100, 100, replace=TRUE)
-    result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
-    coefficients(result1)
-  }
-})[3]
+# perform the bootstrapping in parallel
+r <- foreach(icount(trials), .combine='cbind', .options.mpi=mpiopts) %dopar% {
+  ind <- sample(100, 100, replace=TRUE)
+  result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+  structure(coefficients(result1), names=NULL)
+}
 
-cat(sprintf('Parallel time using doMPI on %d workers: %f\n',
-            getDoParWorkers(), ptime))
+# print the resulting matrix
+print(r)
 
+# shutdown the cluster and finalize MPI
 closeCluster(cl)
 mpi.finalize()
-
-sequential <- as.logical(Sys.getenv('SEQUENTIAL', 'FALSE'))
-if (sequential) {
-  stime <- system.time({
-    r <- foreach(icount(trials), .combine=cbind) %do% {
-      ind <- sample(100, 100, replace=TRUE)
-      result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
-      coefficients(result1)
-    }
-  })[3]
-
-  cat(sprintf('Sequential time: %f\n', stime))
-  cat(sprintf('Speed up for %d workers: %f\n',
-              getDoParWorkers(), round(stime / ptime, digits=2)))
-}
