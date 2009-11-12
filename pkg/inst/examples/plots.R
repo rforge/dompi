6c2f9548ef7bac19ecf10b6992531849ff457f52
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-12 20:50:42 +0000

    Commented, as is fitting for an example.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/plots.R
+++ pkg/inst/examples/plots.R
@@ -1,15 +1,22 @@
 library(doMPI)
 
+# create and register a doMPI cluster
 cl <- startMPIcluster(count=2, verbose=TRUE)
 registerDoMPI(cl)
 
+# initialize variables
 trials <- 10
 n <- nrow(iris)
 leaveout <- 2
+
+# define chunkSize so that each cluster worker gets a single "task chunk"
 chunkSize <- ceiling(trials / getDoParWorkers())
 mpiopts <- list(chunkSize=chunkSize)
 
+# define a .combine function that throws away the "results"
 trash <- function(...) NULL
+
+# create the PNG files in parallel
 foreach(i=icount(trials), .combine=trash, .multicombine=TRUE,
         .packages='randomForest', .options.mpi=mpiopts) %dopar% {
   d <- iris[sample(n, n - leaveout),]
@@ -20,5 +27,6 @@ foreach(i=icount(trials), .combine=trash, .multicombine=TRUE,
   NULL
 }
 
+# shutdown the cluster and finalize MPI
 closeCluster(cl)
 mpi.finalize()
