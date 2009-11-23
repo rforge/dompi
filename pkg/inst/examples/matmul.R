b0905ba8799ace000dffc8379fd2ae419e70e289
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-23 23:35:40 +0000

    Added some comments.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/matmul.R
+++ pkg/inst/examples/matmul.R
@@ -1,8 +1,10 @@
 library(doMPI)
 
+# create and register a doMPI cluster
 cl <- startMPIcluster(count=3)
 registerDoMPI(cl)
 
+# define a parallel matrix multiple function
 matmul <- function(x, y) {
   n <- ceiling(ncol(y) / getDoParWorkers())
   foreach(yc=iter(y, by='column', chunksize=n), .combine='cbind') %dopar% {
@@ -10,13 +12,16 @@ matmul <- function(x, y) {
   }
 }
 
+# create some matrices
 m <- 6; n <- 5; p <- 4
 x <- matrix(rnorm(m * n), m, n)
 y <- matrix(rnorm(n * p), n, p)
 
+# execute matmul and then display and check the result
 z <- matmul(x, y)
 print(z)
 print(identical(z, x %*% y))
 
+# shutdown the cluster and quit
 closeCluster(cl)
 mpi.quit()
