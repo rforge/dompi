6ee2d8fc6ae03b0fe16e41b21bd6d0bd11634077
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-25 02:47:38 +0000

    Seriously modified to allow it to be executed in non-spawn mode. Added command line processing in order to work in this way.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/benchmarks/matmul.R
+++ pkg/inst/benchmarks/matmul.R
@@ -1,19 +1,13 @@
-library(doMPI)
+# This isn't an example.  It's a benchmark, so it has a number
+# of options to control the execution of the benchmark.
+# That makes it too complicated for an example, although the
+# complexity has nothing to do with parallel computing.
+#
+# TODO Document the usage
 
-m <- 2000
-n <- 1200
-p <- 1200
+suppressMessages(library(doMPI))
 
-count <- as.integer(Sys.getenv('COUNT', '3'))
-bcast <- as.logical(Sys.getenv('BCAST', 'TRUE'))
-verbose <- as.logical(Sys.getenv('VERBOSE', 'FALSE'))
-profile <- as.logical(Sys.getenv('PROFILE', 'FALSE'))
-forcePiggyback <- as.logical(Sys.getenv('FORCEPIGGYBACK', 'FALSE'))
-bcastThreshold <- as.integer(Sys.getenv('BCASTTHRESHOLD', '800'))
-
-cl <- startMPIcluster(count=count, bcast=bcast, verbose=verbose)
-registerDoMPI(cl)
-
+# Define a parallel matrix multiply function
 matmul <- function(x, y, profile=FALSE, forcePiggyback=FALSE,
                    bcastThreshold=800) {
   n <- ceiling(ncol(y) / getDoParWorkers())
@@ -25,18 +19,113 @@ matmul <- function(x, y, profile=FALSE, forcePiggyback=FALSE,
   }
 }
 
-x <- matrix(rnorm(m * n), m, n)
-y <- matrix(rnorm(n * p), n, p)
+# Main program executed by master and workers
+main <- function(args) {
+  # Size of the matrices
+  m <- 2000
+  n <- 1200
+  p <- 1200
 
-# gc(FALSE)
-# Rprof()
-stime <- proc.time()[3]
-z <- matmul(x, y, profile, forcePiggyback)
-etime <- proc.time()[3] - stime
-# Rprof(NULL)
+  # Default option values
+  count <- if (mpi.comm.size(0) > 1) {
+    mpi.comm.size(0) - 1
+  } else {
+    mpi.universe.size() - 1
+  }
+  bcast <- TRUE
+  verbose <- FALSE
+  profile <- FALSE
+  forcePiggyback <- FALSE
+  bcastThreshold <- 800
+  cores <- 1
+  wfile <- sprintf("MPI_%d_%s.log", mpi.comm.rank(0), Sys.info()[['user']])
 
-cat(sprintf('Time for matrix multiply with %s and %d workers: %f\n',
-            getDoParName(), getDoParWorkers(), etime))
+  # Process the command line
+  i <- 1
+  while (i <= length(args)) {
+    if (args[i] == '-n') {
+      if (mpi.comm.size(0) > 1)
+        stop('error: -n cannot be used in non-spawning mode')
+      i <- i + 1
+      if (is.na(args[i]))
+        stop('error: -n takes a required numeric argument')
+      count <- suppressWarnings(as.integer(args[i]))
+      if (is.na(count))
+        stop('error: -n takes a numeric argument')
+    } else if (args[i] == '-emulate') {
+      bcast <- FALSE
+    } else if (args[i] == '-v') {
+      verbose <- TRUE
+    } else if (args[i] == '-profile') {
+      profile <- TRUE
+    } else if (args[i] == '-force') {
+      forcePiggyback <- TRUE
+    } else if (args[i] == '-threshold') {
+      i <- i + 1
+      if (is.na(args[i]))
+        stop('error: -threshold takes a required numeric argument')
+      bcastThreshold <- suppressWarnings(as.integer(args[i]))
+      if (is.na(bcastThreshold))
+        stop('error: -threshold takes a numeric argument')
+    } else if (args[i] == '-cores') {
+      i <- i + 1
+      if (is.na(args[i]))
+        stop('error: -cores takes a required numeric argument')
+      cores <- suppressWarnings(as.integer(args[i]))
+      if (is.na(cores))
+        stop('error: -cores takes a numeric argument')
+    } else {
+      stop('error: illegal option: ', args[i])
+    }
+    i <- i + 1
+  }
 
-closeCluster(cl)
+  # Check if this is the master or a cluster worker
+  if (mpi.comm.rank(0) > 0) {
+    # This is a cluster worker
+    outfile <- if (verbose) wfile else "/dev/null"
+    doMPI:::sinkWorkerOutput(outfile)
+    cl <- doMPI:::openMPIcluster(bcast, comm=0L, verbose=verbose)
+    doMPI:::workerLoop(cl, cores=cores, verbose=verbose)
+  } else {
+    if (forcePiggyback) {
+      cat("Broadcasting is disabled: job data will always be piggy-backed\n")
+    } else {
+      if (bcast) {
+        cat("Using true MPI broadcast for job data\n")
+      } else {
+        cat("Using emulated broadcast for job data\n")
+      }
+      cat(sprintf("Piggy-back/broadcast threshold is %d\n", bcastThreshold))
+    }
+
+    # This is the master
+    # Create and register an MPI cluster
+    cl <- startMPIcluster(count=count, bcast=bcast, verbose=verbose)
+    registerDoMPI(cl)
+
+    # Create some matrices
+    x <- matrix(rnorm(m * n), m, n)
+    y <- matrix(rnorm(n * p), n, p)
+
+    # Execute matmul and report the time
+    stime <- proc.time()[3]
+    z <- matmul(x, y, profile, forcePiggyback)
+    etime <- proc.time()[3] - stime
+    cat(sprintf('Time for matrix multiply with %s and %d workers: %f\n',
+                getDoParName(), getDoParWorkers(), etime))
+
+    # Shutdown the cluster
+    closeCluster(cl)
+  }
+}
+
+# Catch errors so we can call mpi.quit
+tryCatch({
+  main(commandArgs(trailingOnly=TRUE))
+},
+error=function(e) {
+  cat(sprintf('%s\n', conditionMessage(e)))
+})
+
 mpi.quit()
