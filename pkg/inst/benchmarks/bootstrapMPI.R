52ea9e68bcdcc4d0b7f7f6a3b270fbd3a2b02332
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-26 02:55:00 +0000

    Started modifying to support non-spawn mode, as in matmul.R.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@95 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/benchmarks/bootstrapMPI.R
+++ pkg/inst/benchmarks/bootstrapMPI.R
@@ -1,4 +1,3 @@
-#
 # NOTE:  I consider this to be a useful benchmark for comparing
 # different parallel systems, but it is important to keep in
 # mind that the tasks are rather small.  That is useful for
@@ -14,61 +13,135 @@
 # system, not the speed of the combine mechanism in the foreach
 # package.
 #
+# TODO Document the usage
 
-library(doMPI)
+suppressMessages(library(doMPI))
 
-usenws <- as.logical(Sys.getenv('USENWS', 'FALSE'))
-count <- as.integer(Sys.getenv('COUNT', '3'))
-verbose <- as.logical(Sys.getenv('VERBOSE', 'FALSE'))
-im <- as.logical(Sys.getenv('INCLUDEMASTER', 'TRUE'))
-maxcores <- as.integer(Sys.getenv('MAXCORES', '1'))
-
-cl <- if (usenws) {
-  startNWScluster(count=count, verbose=verbose, includemaster=im,
-                  maxcores=maxcores)
-} else {
-  startMPIcluster(count=count, verbose=verbose, includemaster=im,
-                  maxcores=maxcores)
-}
-registerDoMPI(cl)
-
-x <- iris[which(iris[,5] != "setosa"), c(1,5)]
-trials <- 10000
-
-chunking <- as.logical(Sys.getenv('CHUNKING', 'TRUE'))
-chunkSize <- if (chunking) ceiling(trials / getDoParWorkers()) else 1
-mpiopts <- list(chunkSize=chunkSize)
-trash <- function(...) NULL
-
-ptime <- system.time({
-  foreach(icount(trials), .combine=trash, .multicombine=TRUE,
-               .options.mpi=mpiopts) %dopar% {
-    ind <- sample(100, 100, replace=TRUE)
-    result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
-    coefficients(result1)
+main <- function(args) {
+  count <- if (mpi.comm.size(0) > 1) {
+    mpi.comm.size(0) - 1
+  } else {
+    mpi.universe.size() - 1
   }
-})[3]
+  bcast <- TRUE
+  verbose <- FALSE
+  profile <- FALSE
+  forcePiggyback <- FALSE
+  bcastThreshold <- 800
+  cores <- 1
+  sequential <- TRUE
+  chunking <- TRUE
+  wfile <- sprintf("MPI_%d_%s.log", mpi.comm.rank(0), Sys.info()[['user']])
 
-cat(sprintf('Parallel time using doMPI on %d workers: %f\n',
-            getDoParWorkers(), ptime))
-
-closeCluster(cl)
-
-sequential <- as.logical(Sys.getenv('SEQUENTIAL', 'FALSE'))
-if (sequential) {
-  stime <- system.time({
-    foreach(icount(trials), .combine=trash, .multicombine=TRUE) %do% {
-      ind <- sample(100, 100, replace=TRUE)
-      result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
-      coefficients(result1)
+  # Process the command line
+  i <- 1
+  while (i <= length(args)) {
+    if (args[i] == '-n') {
+      if (mpi.comm.size(0) > 1)
+        stop('error: -n cannot be used in non-spawning mode')
+      i <- i + 1
+      if (is.na(args[i]))
+        stop('error: -n takes a required numeric argument')
+      count <- suppressWarnings(as.integer(args[i]))
+      if (is.na(count))
+        stop('error: -n takes a numeric argument')
+    } else if (args[i] == '-emulate') {
+      bcast <- FALSE
+    } else if (args[i] == '-v') {
+      verbose <- TRUE
+    } else if (args[i] == '-profile') {
+      profile <- TRUE
+    } else if (args[i] == '-force') {
+      forcePiggyback <- TRUE
+    } else if (args[i] == '-threshold') {
+      i <- i + 1
+      if (is.na(args[i]))
+        stop('error: -threshold takes a required numeric argument')
+      bcastThreshold <- suppressWarnings(as.integer(args[i]))
+      if (is.na(bcastThreshold))
+        stop('error: -threshold takes a numeric argument')
+    } else if (args[i] == '-cores') {
+      i <- i + 1
+      if (is.na(args[i]))
+        stop('error: -cores takes a required numeric argument')
+      cores <- suppressWarnings(as.integer(args[i]))
+      if (is.na(cores))
+        stop('error: -cores takes a numeric argument')
+    } else {
+      stop('error: illegal option: ', args[i])
     }
-  })[3]
+    i <- i + 1
+  }
 
-  cat(sprintf('Sequential time: %f\n', stime))
-  cat(sprintf('Speed up for %d workers: %f\n',
-              getDoParWorkers(), round(stime / ptime, digits=2)))
+  # Check if this is the master or a cluster worker
+  if (mpi.comm.rank(0) > 0) {
+    # This is a cluster worker
+    outfile <- if (verbose) wfile else "/dev/null"
+    sinkWorkerOutput(outfile)
+    cl <- openMPIcluster(bcast=bcast)
+    workerLoop(cl, cores=cores, verbose=verbose)
+  } else {
+    if (forcePiggyback) {
+      cat("Broadcasting is disabled: job data will always be piggy-backed\n")
+    } else {
+      if (bcast) {
+        cat("Using true MPI broadcast for job data\n")
+      } else {
+        cat("Using emulated broadcast for job data\n")
+      }
+      cat(sprintf("Piggy-back/broadcast threshold is %d\n", bcastThreshold))
+    }
+
+    # This is the master
+    # Create and register an MPI cluster
+    cl <- startMPIcluster(count=count, bcast=bcast, verbose=verbose)
+    registerDoMPI(cl)
+
+    x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+    trials <- 10000
+
+    chunkSize <- if (chunking) ceiling(trials / getDoParWorkers()) else 1
+    opts <- list(chunkSize=chunkSize, profile=profile,
+                 forcePiggyback=forcePiggyback,
+                 bcastThreshold=bcastThreshold)
+    trash <- function(...) NULL
+
+    ptime <- system.time({
+      foreach(icount(trials), .combine=trash, .multicombine=TRUE,
+                   .options.mpi=opts) %dopar% {
+        ind <- sample(100, 100, replace=TRUE)
+        result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+        coefficients(result1)
+      }
+    })[3]
+
+    cat(sprintf('Parallel time using doMPI on %d workers: %f\n',
+                getDoParWorkers(), ptime))
+
+    closeCluster(cl)
+
+    if (sequential) {
+      stime <- system.time({
+        foreach(icount(trials), .combine=trash, .multicombine=TRUE) %do% {
+          ind <- sample(100, 100, replace=TRUE)
+          result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+          coefficients(result1)
+        }
+      })[3]
+
+      cat(sprintf('Sequential time: %f\n', stime))
+      cat(sprintf('Speed up for %d workers: %f\n',
+                  getDoParWorkers(), round(stime / ptime, digits=2)))
+    }
+  }
 }
 
-if (!usenws) {
-  mpi.quit()
-}
+# Catch errors so we can call mpi.quit
+tryCatch({
+  main(commandArgs(trailingOnly=TRUE))
+},
+error=function(e) {
+  cat(sprintf('%s\n', conditionMessage(e)))
+})
+
+mpi.quit()
