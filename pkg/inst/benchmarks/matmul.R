d5d50c7a02c6b985dc0570693a6014cb244b1edf
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-27 03:47:45 +0000

    Rewrote using the getopt package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/benchmarks/matmul.R
+++ pkg/inst/benchmarks/matmul.R
@@ -6,6 +6,7 @@
 # TODO Document the usage
 
 suppressMessages(library(doMPI))
+suppressMessages(library(getopt))
 
 # Define a parallel matrix multiply function
 matmul <- function(x, y, profile=FALSE, forcePiggyback=FALSE,
@@ -21,96 +22,52 @@ matmul <- function(x, y, profile=FALSE, forcePiggyback=FALSE,
 
 # Main program executed by master and workers
 main <- function(args) {
-  # Size of the matrices
-  m <- 2000
-  n <- 1200
-  p <- 1200
-
-  # Default option values
-  count <- if (mpi.comm.size(0) > 1) {
-    mpi.comm.size(0) - 1
-  } else {
-    mpi.universe.size() - 1
-  }
-  bcast <- TRUE
-  verbose <- FALSE
-  profile <- FALSE
-  forcePiggyback <- FALSE
-  bcastThreshold <- 800
-  cores <- 1
-  wfile <- sprintf("MPI_%d_%s.log", mpi.comm.rank(0), Sys.info()[['user']])
-
-  # Process the command line
-  i <- 1
-  while (i <= length(args)) {
-    if (args[i] == '-n') {
-      if (mpi.comm.size(0) > 1)
-        stop('error: -n cannot be used in non-spawning mode')
-      i <- i + 1
-      if (is.na(args[i]))
-        stop('error: -n takes a required numeric argument')
-      count <- suppressWarnings(as.integer(args[i]))
-      if (is.na(count))
-        stop('error: -n takes a numeric argument')
-    } else if (args[i] == '-emulate') {
-      bcast <- FALSE
-    } else if (args[i] == '-v') {
-      verbose <- TRUE
-    } else if (args[i] == '-profile') {
-      profile <- TRUE
-    } else if (args[i] == '-force') {
-      forcePiggyback <- TRUE
-    } else if (args[i] == '-threshold') {
-      i <- i + 1
-      if (is.na(args[i]))
-        stop('error: -threshold takes a required numeric argument')
-      bcastThreshold <- suppressWarnings(as.integer(args[i]))
-      if (is.na(bcastThreshold))
-        stop('error: -threshold takes a numeric argument')
-    } else if (args[i] == '-cores') {
-      i <- i + 1
-      if (is.na(args[i]))
-        stop('error: -cores takes a required numeric argument')
-      cores <- suppressWarnings(as.integer(args[i]))
-      if (is.na(cores))
-        stop('error: -cores takes a numeric argument')
-    } else {
-      stop('error: illegal option: ', args[i])
-    }
-    i <- i + 1
-  }
+  spec <- matrix(c('verbose',   'v', '0', 'logical',
+                   'count',     'n', '1', 'integer',
+                   'profile',   'p', '0', 'logical',
+                   'emulate',   'e', '0', 'logical',
+                   'force',     'f', '0', 'logical',
+                   'threshold', 't', '1', 'integer',
+                   'cores',     'c', '1', 'integer'), ncol=4, byrow=TRUE)
+  options <- getopt(spec, opt=args, command='matmul.R')
+  size <- if (mpi.comm.size(0) > 1) mpi.comm.size(0) else mpi.universe.size()
+  opt <- list(verbose=FALSE, count=size-1, profile=FALSE, emulate=FALSE,
+              force=FALSE, threshold=800, cores=1)
+  opt[names(options)] <- options
 
   # Check if this is the master or a cluster worker
   if (mpi.comm.rank(0) > 0) {
     # This is a cluster worker
-    outfile <- if (verbose) wfile else "/dev/null"
+    wfile <- sprintf("MPI_%d_%s.log", mpi.comm.rank(0), Sys.info()[['user']])
+    outfile <- if (opt$verbose) wfile else "/dev/null"
     sinkWorkerOutput(outfile)
-    cl <- openMPIcluster(bcast=bcast)
-    workerLoop(cl, cores=cores, verbose=verbose)
+    cl <- openMPIcluster(bcast=!opt$emulate)
+    workerLoop(cl, cores=opt$cores, verbose=opt$verbose)
   } else {
-    if (forcePiggyback) {
-      cat("Broadcasting is disabled: job data will always be piggy-backed\n")
-    } else {
-      if (bcast) {
-        cat("Using true MPI broadcast for job data\n")
-      } else {
-        cat("Using emulated broadcast for job data\n")
-      }
-      cat(sprintf("Piggy-back/broadcast threshold is %d\n", bcastThreshold))
-    }
-
-    # This is the master
     # Create and register an MPI cluster
-    cl <- startMPIcluster(count=count, bcast=bcast, verbose=verbose)
+    cl <- startMPIcluster(count=opt$count, bcast=!opt$emulate, verbose=opt$verbose)
     registerDoMPI(cl)
 
+    # Display a summary of how we're going to run the benchmark
+    if (opt$force) {
+      cat("Broadcasting is disabled: job data will always be piggy-backed\n")
+    } else {
+      if (opt$emulate) {
+        cat("Using emulated broadcast for job data\n")
+      } else {
+        cat("Using true MPI broadcast for job data\n")
+      }
+      cat(sprintf("Piggy-back/broadcast threshold is %d\n", opt$threshold))
+    }
+
     # Create some matrices
+    m <- 2000; n <- 1200; p <- 1200
     x <- matrix(rnorm(m * n), m, n)
     y <- matrix(rnorm(n * p), n, p)
 
     # Execute matmul and report the time
     stime <- proc.time()[3]
-    z <- matmul(x, y, profile, forcePiggyback)
+    z <- matmul(x, y, opt$profile, opt$force, opt$threshold)
     etime <- proc.time()[3] - stime
     cat(sprintf('Time for matrix multiply with %s and %d workers: %f\n',
                 getDoParName(), getDoParWorkers(), etime))
