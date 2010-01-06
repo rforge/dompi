562f5d94e863c1a1dda7c9ee967f5dac03310ae0
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-06 13:42:25 +0000

    Modified to reflect Erin's recent changes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@142 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/arima.R
+++ pkg/inst/examples/arima.R
@@ -10,8 +10,9 @@ opts <- list(chunkSize=100)
 
 # Perform simulations in parallel
 r <- foreach(1:n, .combine='c', .options.mpi=opts) %dopar% {
-  x <- arima.sim(list(order=c(1,0,0), ar=-0.9), n=120)
-  arima(x, order=c(1,0,0))$model$phi
+  x <- ts(arima.sim(list(order=c(1,0,0), ar=-0.9), n=360), start=1975, freq=12)
+  y <- aggregate(x, nfreq=4, sum)
+  arima(y, order=c(1,0,0))$model$phi
 }
 
 # Print a summary of the resulting vector
