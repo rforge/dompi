54335f4352ea1e03373cee199e4cefc47af1ef7a
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-28 00:05:07 +0000

    Updated to use setDoMpiRNG, rather than rsprng.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/prng.R
+++ pkg/inst/examples/prng.R
@@ -1,6 +1,6 @@
 # This example shows how to setup the cluster workers
 # to use parallel random number generation using the
-# rsprng package.
+# new setDoMpiRNG function.
 
 suppressMessages(library(doMPI))
 
@@ -8,32 +8,18 @@ suppressMessages(library(doMPI))
 cl <- startMPIcluster()
 registerDoMPI(cl)
 
-# Initialize the workers to use rsprng for parallel random number
-# generation.  This implementation assumes that if there are N workers,
-# the first N tasks will all be executed by different workers, thus
-# initializing all N workers.  That is true for doMPI, but isn't a
-# general requirement for foreach backends.  This implementation also
-# assumes that there is a persistent set of workers.  Again, this is
-# true for doMPI, but not for doMC in particular.
-initSPRNG <- function(seed=8765) {
-  nstream <- getDoParWorkers()
-  foreach(streamno=seq_len(nstream)-1L, .packages='rsprng') %dopar% {
-    .Call('r_init_sprng', 0L, as.integer(streamno), as.integer(nstream),
-          as.integer(seed), 0L, PACKAGE='rsprng')
-    RNGkind('user')
-    NULL
+# Check if we get "repeatable" results
+for (n in 1:3) {
+  # Initialize parallel RNG
+  setDoMpiRNG(cl, seed=1234)
+
+  # Execute a foreach loop that uses random numbers
+  r <- foreach(i=icount(clusterSize(cl)), .combine='c') %dopar% {
+    Sys.sleep(3)
+    as.integer(runif(1, max=1000))
   }
-  invisible()
+  print(r)
 }
-
-# Initialize parallel RNG
-initSPRNG(7777442)
-
-# Execute a foreach loop that uses random numbers
-r <- foreach(i=icount(100), .combine='c') %dopar% {
-  as.integer(runif(1, max=1000))
-}
-print(r)
 
 # Shutdown the cluster and quit
 closeCluster(cl)
