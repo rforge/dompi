90bf6f2524f5947445bf75611dda2504098b92b8
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-12 19:21:33 +0000

    Improved error handling.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@180 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rng.R
+++ pkg/R/rng.R
@@ -4,32 +4,33 @@
 #
 setRngDoMPI <- function(cl, seed=NULL) {
   # save the current value of .Random.seed so it can be restored
-  if (exists('.Random.seed', where=globalenv(), inherits=FALSE))
-    saveseed <- get('.Random.seed', pos=globalenv(), inherits=FALSE)
-  else
-    saveseed <- NULL
+  saveseed <- if (exists('.Random.seed', where=globalenv(), inherits=FALSE))
+    get('.Random.seed', pos=globalenv(), inherits=FALSE)
 
   # set RNG to L'Ecuyer in order to generate .Random.seed values
   # to send to the workers, saving the previous value
   saverng <- RNGkind("L'Ecuyer-CMRG")
 
-  # call set.seed if seed is not NULL
-  if (! is.null(seed))
-    set.seed(seed)
+  tryCatch({
+    # call set.seed if seed is not NULL
+    if (! is.null(seed))
+      set.seed(seed)
 
-  # send a .Random.seed value to each worker in the cluster
-  s <- .Random.seed
-  for (i in seq(length=clusterSize(cl))) {
-    s <- nextRNGStream(s)
-    sendToWorker(cl, i, list(seed=s))
-  }
-
-  # restore the local RNG and .Random.seed
-  RNGkind(saverng[1])
-  if (is.null(saveseed))
-    rm('.Random.seed', pos=globalenv())
-  else
-    assign('.Random.seed', saveseed, pos=globalenv())
+    # send a .Random.seed value to each worker in the cluster
+    s <- get('.Random.seed', pos=globalenv(), inherits=FALSE)
+    for (i in seq(length=clusterSize(cl))) {
+      s <- nextRNGStream(s)
+      sendToWorker(cl, i, list(seed=s))
+    }
+  },
+  finally={
+    # restore the local RNG and .Random.seed
+    RNGkind(saverng[1], saverng[2])
+    if (is.null(saveseed))
+      rm('.Random.seed', pos=globalenv())
+    else
+      assign('.Random.seed', saveseed, pos=globalenv())
+  })
 
   invisible(NULL)
 }
