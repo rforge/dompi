1a2f415ebfa10171f970e323e7ddc1c8710db48e
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-15 23:37:14 +0000

    Improved the description.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@194 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/prng.R
+++ pkg/inst/examples/prng.R
@@ -2,7 +2,8 @@
 # random number generation using the new setRngDoMPI function.  It
 # causes each worker to produce independent random numbers, but the
 # results aren't necessarily reproducible since the tasks can be
-# executed by different workers due to load imbalance for example.
+# executed by different workers on different runs.  The results will
+# also be different for different numbers of workers.
 
 suppressMessages(library(doMPI))
 
@@ -11,13 +12,14 @@ cl <- startMPIcluster()
 registerDoMPI(cl)
 
 n <- 3
+w <- clusterSize(cl)
 
-cat(sprintf("Only first %d results are guaranteed repeatable\n", clusterSize(cl)))
+cat(sprintf("Only first %d results are guaranteed repeatable\n", w))
 fun <- function(ignore) {
   # Initialize parallel RNG
   setRngDoMPI(cl, seed=42)
 
-  foreach(sleep=irunif(1, max=5, count=4 * clusterSize(cl)),
+  foreach(sleep=irunif(1, max=5, count=4*w),
           .combine='rbind') %dopar% {
     Sys.sleep(sleep)
     data.frame(rank=mpi.comm.rank(0), result=as.integer(runif(1, max=1000)))
