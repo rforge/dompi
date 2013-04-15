cf7084f0f2525da9a36c576d2d132f46d0f84162
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-15 22:53:41 +0000

    Changed format of results to show worker rank.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@193 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/prng.R
+++ pkg/inst/examples/prng.R
@@ -1,6 +1,8 @@
-# This example shows how to setup the cluster workers
-# to use parallel random number generation using the
-# new setRngDoMPI function.
+# This example shows how to setup the cluster workers to use parallel
+# random number generation using the new setRngDoMPI function.  It
+# causes each worker to produce independent random numbers, but the
+# results aren't necessarily reproducible since the tasks can be
+# executed by different workers due to load imbalance for example.
 
 suppressMessages(library(doMPI))
 
@@ -8,33 +10,21 @@ suppressMessages(library(doMPI))
 cl <- startMPIcluster()
 registerDoMPI(cl)
 
-# Verify that we get repeatable results
-cat("Repeatable results\n")
-for (n in 1:3) {
-  # Initialize parallel RNG
-  setRngDoMPI(cl, seed=42)
+n <- 3
 
-  r <- foreach(sleep=irunif(1, max=5, count=clusterSize(cl)),
-               .combine='c') %dopar% {
-    Sys.sleep(sleep)
-    as.integer(runif(1, max=1000))
-  }
-  print(r)
-}
-
-# Check that we don't get repeatable results due to load imbalance
 cat(sprintf("Only first %d results are guaranteed repeatable\n", clusterSize(cl)))
-for (n in 1:3) {
+fun <- function(ignore) {
   # Initialize parallel RNG
   setRngDoMPI(cl, seed=42)
 
-  r <- foreach(sleep=irunif(1, max=5, count=4 * clusterSize(cl)),
-               .combine='rbind') %dopar% {
+  foreach(sleep=irunif(1, max=5, count=4 * clusterSize(cl)),
+          .combine='rbind') %dopar% {
     Sys.sleep(sleep)
     data.frame(rank=mpi.comm.rank(0), result=as.integer(runif(1, max=1000)))
   }
-  print(r)
 }
+r <- lapply(1:n, fun)
+print(do.call('cbind', r))
 
 # Shutdown the cluster and quit
 closeCluster(cl)
--- pkg/inst/examples/prng2.R
+++ pkg/inst/examples/prng2.R
@@ -16,13 +16,16 @@ initEnvir <- function(envir) RNGkind("L'Ecuyer-CMRG")
 mpiopts <- list(initEnvir=initEnvir)
 
 # Verify that we get repeatable results
+nc <- 4
+nr <- 20
+isleep <- irunif(1, max=5)
 r <-
-  foreach(1:4, .combine='cbind') %:%
-    foreach(sleep=irunif(1, max=5, count=18), seed=iRNGSubStream(42),
-            .combine='c', .options.mpi=mpiopts) %dopar% {
+  foreach(1:nc, .combine='cbind') %:%
+    foreach(1:nr, sleep=isleep, seed=iRNGSubStream(42),
+            .combine='rbind', .options.mpi=mpiopts) %dopar% {
       assign('.Random.seed', seed, pos=.GlobalEnv)
       Sys.sleep(sleep)  # Randomize task length
-      as.integer(runif(1, max=1000))
+      data.frame(rank=mpi.comm.rank(0), result=as.integer(runif(1, max=1000)))
     }
 
 print(r)
