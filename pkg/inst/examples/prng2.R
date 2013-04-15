fa842a06196371a218288904495869e6f4a18627
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-15 23:47:09 +0000

    Improved the description.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dompi@195 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/examples/prng2.R
+++ pkg/inst/examples/prng2.R
@@ -1,8 +1,12 @@
-# This example shows how to setup the cluster workers
-# to use parallel random number generation using the
-# itertools "iRNGSubStream" function.
-#
-# This uses an approach inspired by the "doRNG" package.
+# This example shows how to setup the cluster workers to use parallel
+# random number generation using the itertools "iRNGSubStream" function.
+# It uses a different random number substream for each task so that the
+# results are repeatable regardless of the number of workers or which
+# worker executes what task.  Note that the results show that the
+# task results are the same on subsequent runs even though different
+# workers generate those results.
+# 
+# The approach was inspired by the "doRNG" package.
 
 suppressMessages(library(doMPI))
 library(itertools)
@@ -23,7 +27,7 @@ r <-
   foreach(1:nc, .combine='cbind') %:%
     foreach(1:nr, sleep=isleep, seed=iRNGSubStream(42),
             .combine='rbind', .options.mpi=mpiopts) %dopar% {
-      assign('.Random.seed', seed, pos=.GlobalEnv)
+      assign('.Random.seed', seed, pos=globalenv())
       Sys.sleep(sleep)  # Randomize task length
       data.frame(rank=mpi.comm.rank(0), result=as.integer(runif(1, max=1000)))
     }
